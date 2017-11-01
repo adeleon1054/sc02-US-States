83fcd7f3376b613be1b08201ecd26bc9044f26f8
Asdruval De Leon <adeleon1054@gmail.com>
2017-11-01 18:34:21 -0400

    Initial Commit

---
+++ sc02-US-States/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  sc02-US-States
+//
+//  Created by Asdruval De Leon on 11/1/17.
+//  Copyright © 2017 Asdruval De Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ sc02-US-States/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  sc02-US-States
+//
+//  Created by Asdruval De Leon on 11/1/17.
+//  Copyright © 2017 Asdruval De Leon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
